1a3c364963e63885e7307905f130229d3bfcab98
SuperCheese21 <ethan.shields21@gmail.com>
2021-07-24 18:46:20 -0700

    Changed script to navigate directly to search results

--- src/purchaseItem.js
+++ src/purchaseItem.js
@@ -2,14 +2,11 @@ import puppeteer from 'puppeteer';
 
 (async () => {
   const query = 'microphone';
+  const url = `https://www.bestbuy.com/site/searchpage.jsp?st=${query}&_dyncharset=UTF-8&_dynSessConf=&id=pcat17071&type=page&sc=Global&cp=1&nrp=&sp=&qp=&list=n&af=true&iht=y&usc=All+Categories&ks=960&keys=keys`;
 
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   page.setViewport({ width: 1000, height: 660 });
 
-  await page.goto('https://www.bestbuy.com/', {
-    waitUntil: ['domcontentloaded', 'networkidle0'],
-  });
-  await page.click('.c-modal-close-icon');
-  await page.type('#gh-search-input', query);
+  await page.goto(url, { waitUntil: 'domcontentloaded' });
 })();
