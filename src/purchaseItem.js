aadf831f2bf195722394f3c49492d6d17dcc45be
SuperCheese21 <ethan.shields21@gmail.com>
2021-07-24 18:23:42 -0700

    Added launch.json for vscode debugger

--- src/purchaseItem.js
+++ src/purchaseItem.js
@@ -1,6 +1,15 @@
-import axios from 'axios';
+import puppeteer from 'puppeteer';
 
 (async () => {
-  const res = await axios.get('https://www.bestbuy.com/');
-  console.log({ res });
+  const query = 'microphone';
+
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  page.setViewport({ width: 1000, height: 660 });
+
+  await page.goto('https://www.bestbuy.com/', {
+    waitUntil: ['domcontentloaded', 'networkidle0'],
+  });
+  await page.click('.c-modal-close-icon');
+  await page.type('#gh-search-input', query);
 })();
